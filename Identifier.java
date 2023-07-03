36853176b93b4543e522f659bc8283ef2b7a306c
Rashi1703 <128238586+Rashi1703@users.noreply.github.com>
2023-07-03 23:02:47 +0530

    Create Identifier.java

---
+++ Identifier.java
@@ -0,0 +1,11 @@
+public class Identifier {
+
+    public static void main(String[] args) {
+        int myNum = 15;
+        System.out.println(myNum);
+        double pi=3.14d;
+        System.out.println(myNum);
+        String name = "Rashi";
+        System.out.println(name);
+    }
+}
